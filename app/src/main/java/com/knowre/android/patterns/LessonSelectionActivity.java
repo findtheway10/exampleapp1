7f71c3ac342fb8e157d9d5c9622a051fcc3b2049
taenguree <mibeeya@gmail.com>
2018-05-29 22:12:19 +0900

    implementing problem solving activity layout

--- app/src/main/java/com/knowre/android/patterns/LessonSelectionActivity.java
+++ app/src/main/java/com/knowre/android/patterns/LessonSelectionActivity.java
@@ -1,5 +1,6 @@
 package com.knowre.android.patterns;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -9,6 +10,10 @@ public class LessonSelectionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lesson_selection);
+
+        findViewById(R.id.basic_lesson).setOnClickListener(v -> {
+            startActivity(new Intent(this, ProblemSolvingActivity.class));
+        });
     }
 
 }
