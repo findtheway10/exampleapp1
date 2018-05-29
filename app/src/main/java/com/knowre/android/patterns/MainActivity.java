c3a06ca2d8b1ce2614f83f01f133d44ad4bff375
taenguree <mibeeya@gmail.com>
2018-05-29 21:22:26 +0900

    implemeting lesson selection layout

--- app/src/main/java/com/knowre/android/patterns/MainActivity.java
+++ app/src/main/java/com/knowre/android/patterns/MainActivity.java
@@ -1,5 +1,6 @@
 package com.knowre.android.patterns;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -9,6 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.do_login).setOnClickListener(v -> {
+            startActivity(new Intent(this, LessonSelectionActivity.class));
+        });
     }
 
 }
