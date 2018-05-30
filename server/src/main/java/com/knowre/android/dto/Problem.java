7db30fb1f6b23ba41411d9d7bf46d78086cf20e6
taenguree <mibeeya@gmail.com>
2018-05-30 16:54:57 +0900

    responding problem number on remote call

--- server/src/main/java/com/knowre/android/dto/Problem.java
+++ server/src/main/java/com/knowre/android/dto/Problem.java
@@ -2,12 +2,13 @@ package com.knowre.android.dto;
 
 public final class Problem {
 
+    public final int problemNumber;
     public final String problemContent;
     public final String correctAnswer;
 
-    public Problem(String problemContent, String correctAnswer) {
+    public Problem(int problemNumber, String problemContent, String correctAnswer) {
+        this.problemNumber = problemNumber;
         this.problemContent = problemContent;
         this.correctAnswer = correctAnswer;
     }
-
 }
